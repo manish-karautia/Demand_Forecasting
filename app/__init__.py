5c59167a8eac8030e26f8e62d2923c7e8855c203
Manish <manishkarautia@gmail.com>
2025-11-14 14:45:00 +0530

    Fix issue for CI/CD pipeline

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
-"""from flask import Flask 
+from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy 
 from flask_login import LoginManager 
+from app.database import db 
 
-db = SQLAlchemy() 
 login_manager = LoginManager() 
 
 def create_app():
@@ -20,34 +20,7 @@ def create_app():
   
   from .auth import auth as auth_blueprint 
   app.register_blueprint(auth_blueprint) 
-  return app"""
+  return app
   
-import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 
-db = SQLAlchemy()
-login_manager = LoginManager()
 
-def create_app():
-    app = Flask(__name__)
-
-    # --- FIX: ALWAYS PUT SQLITE DB INSIDE INSTANCE FOLDER ---
-    instance_path = os.path.join(app.root_path, '..', 'instance')
-    os.makedirs(instance_path, exist_ok=True)
-    db_path = os.path.join(instance_path, 'users.db')
-
-    app.config['SECRET_KEY'] = 'demo-secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
-
-    db.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = 'auth.login'
-
-    from .routes import main as main_blueprint
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(main_blueprint)
-    app.register_blueprint(auth_blueprint)
-
-    return app
